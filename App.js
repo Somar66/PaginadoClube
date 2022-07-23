5ed9199183760b4f2268f71dba47e499d179a88f
André Ramos <106190579+Somar66@users.noreply.github.com>
2022-07-23 15:10:46 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import './App.css'
+
+function App () {
+return (
+
+<div className="html">
+  <div className="head">
+    <link rel="stylesheet" href="styles.css" />
+  </div>
+  <body>
+    <nav id="navbar">
+      <header><img id="logo" src="https://www.zerozero.pt/img/logos/equipas/6742_imgbank.png" />Biblioteca Instrução e Recreio</header>
+      <ul>
+        <li><a className="nav-link" href="#História_do_clube">História do clube</a></li>
+        <li>
+          <a className="nav-link" href="#Hóquei_em_Patins"
+            >Hóquei em Patins </a>
+        </li>
+        <li>
+          <a className="nav-link" href="#Patinagem_Artística"
+            >Patinagem Artística</a>
+        </li>
+        <li><a className="nav-link" href="#Futebol">Futebol</a></li>
+        <li><a className="nav-link" href="#Basquetebol">Basquetebol</a></li>
+        <li>
+          <a className="nav-link" href="#Jazz"
+            >Jazz</a>
+        </li>        
+      </ul>
+    </nav>
+    <main id="main-doc">
+      <section className="main-section" id="História_do_clube">
+        <header>História do clube</header>
+        <article>
+          <p>
+            A Biblioteca de Instrução e Recreio (BIR), fundada em 1933 para combater o analfabetismo da população rural existente na época em Valado dos Frades, é uma Colectividade de Utilidade Pública que ao longo dos anos se tem afirmado como principal responsável pelas atividades de caráter cultural e desportivo da vila. Das atividades culturais que desenvolve, destacam-se a exposição etnográfica e histórica da vida do Valado e da Biblioteca, exposições de pintura com artistas locais, concertos do grupo de música popular portuguesa.
+          </p>
+        </article>
+        <code> #LuzSempreMaisLuz</code>
+      </section>
+      <section className="main-section" id="Hóquei_em_Patins">
+        <header><li>Hóquei em Patins</li></header>
+        <article>
+          <p>
+          O Hóquei em patins é um desporto coletivo que se joga num rinque em que os atletas rolam sobre patins e usam um stick para conduzir uma bola e introduzi-la na baliza adversária para marcar um golo.
+</p>
+<p>
+O hóquei em patins é jogado por cinco jogadores, quatro em campo e um guarda-redes. Neste desporto é utilizado o seguinte equipamento: patins (compostos por quatro rodas cada patim, tacão, chassi e bota), stick e bola. Como equipamento de proteção utiliza-se caneleiras, joelheiras, luvas e coquilha (para os homens e para mulheres) e no caso dos guarda-redes proteção de coxa, máscara para a cabeça, cotoveleiras, luvas e peito. O jogo disputa-se num rinque.
+          </p>
+          <code> #LuzSempreMaisLuz</code>
+        </article>
+      </section>
+      <section className="main-section" id="Patinagem_Artística">
+        <header><li>Patinagem Artística</li></header>
+        <article>
+          <p>
+            A patinagem artística sobre rodas ou patinação artística de rodas (português do Brasil) é um desporto semelhante à patinação artística no gelo, mas onde os competidores usam patins com quatro rodas em vez de patins com lâminas côncavas. Na patinagem artística, existem várias disciplinas: Figuras Obrigatórias, Patinagem Livre, Pares Artísticos, Quartetos, Show & Precisão, Solo Dance e Pares de Dança.
+          </p>
+          <code> #LuzSempreMaisLuz</code>
+        </article>
+      </section>
+      <section className="main-section" id="Futebol">
+        <header><li>Futebol</li></header>
+        <article>
+         <p>
+           O futebol, também referido como futebol de campo, futebol de onze e, controversamente, futebol associado (em inglês: association football, football, soccer), é um desporto de equipe jogado entre dois times de 11 jogadores cada um e um árbitro que se ocupa da correta aplicação das normas. É considerado o desporto mais popular do mundo, pois cerca de 270 milhões de pessoas participam das suas várias competições. É jogado num campo retangular gramado, com uma baliza em cada lado do campo. O objetivo do jogo é deslocar uma bola através do campo para colocá-la dentro da baliza adversária, ação que se denomina golo (português europeu) ou gol (português brasileiro). A equipe que marca mais gols ao término da partida é a vencedora.
+</p>
+<p>
+O jogo moderno foi criado na Inglaterra com a formação da The Football Association, cujas regras de 1863 são a base do desporto na atualidade. O órgão regente do futebol é a Federação Internacional de Futebol (em francês: Fédération Internationale de Football Association), mais conhecida pela sigla FIFA. A principal competição internacional de futebol é a Copa do Mundo FIFA, realizada a cada quatro anos. Este evento é o mais famoso e com maior quantidade de espectadores do mundo, o dobro da audiência dos Jogos Olímpicos.
+           </p>
+           <code> #LuzSempreMaisLuz</code>
+        </article>
+      </section>
+      <section className="main-section" id="Basquetebol">
+        <header><li>Basquetebol</li></header>
+        <p>
+         O basquetebol ou bola ao cesto, também conhecido como "basquete". É um jogo desportivo coletivo inventado em 1891 pelo professor de Educação Física canadense James Naismit, na Associação Cristã de Rapazes de Springfield, Massachusetts, Estados Unidos. É disputado por duas equipes de 12 jogadores (5 em campo e 7 suplentes) que têm como objetivo passar a bola por dentro de um cesto e evitar que a bola entre no seu cesto colocado nas extremidades da quadra, seja num ginásio ou ao ar livre.
+</p>
+<p>
+Os aros que formam os cestos são colocados a uma altura de 3 metros e 5 centímetros. Os jogadores podem caminhar no campo desde que driblem (batam a bola contra o chão) a cada passo dado. Também é possível executar um passe, ou seja, passar a bola em direção a um companheiro de equipe.
+</p>
+<p>
+O basquetebol é um desporto olímpico desde os Jogos Olímpicos de Verão de 1936 em Berlim.
+        </p>
+        <code> #LuzSempreMaisLuz</code>
+      </section>
+      <section className="main-section" id="Jazz">
+        <header><li>Jazz</li></header>
+        <article className="abobora">
+          Jazz na terra da abóbora...
+          
+        </article>
+          <p>
+            Quando, em 1985, Adelino Mota, então estudante em Coimbra, colocou ao contrabaixista David Gausden a possibilidade de tocar no verão seguinte na pequena povoação de Valado dos Frades – de feição vincadamente rural, situada entre a Nazaré e Alcobaça – estaria longe de antever que, quase 30 anos volvidos, o Festival de Jazz de Valado dos Frades fosse considerado, por músicos, críticos e público, como um dos mais carismáticos festivais nacionais de jazz.
+</p>
+<p>
+O autor recorre a depoimentos de alguns dos músicos com uma ligação mais próxima ao festival, como são os casos de André Sousa Machado (o músico que mais vezes lá tocou), Bruno Pedroso, Sérgio Carolino, Nanã Sousa Dias, Carlos Barretto, Claus Nymark, João e Bernardo Moreira, Jacinta e Alexandre Frazão. Galego não esquece o impacto regional gerado, de que resulta, por exemplo, a criação da Big Band do Município da Nazaré (constituída sobretudo por músicos desta localidade e do Valado), as opiniões dos principais músicos estrangeiros que nele tocaram e também todo o ambiente muito especial que se vive em torno deste evento.
+          </p>
+          <p>
+            
+Com a sua atmosfera única, sob a forma de um clube de jazz que abre anualmente, o Festival do Valado é hoje um ponto de peregrinação essencial no roteiro dos muitos festivais que têm lugar por terras lusas. É essa história, feita de estórias e memórias, que o jornalista Mário Galego procura desfiar em “Uma Estória de Jazz em Valado dos Frades”, edição conjunta, agora revista e aumentada, da Biblioteca Instrução e Recreio (BIR) – entidade fundada em 1933 que organiza o festival – e da editora Ediresistência, com apoio do Jazz ao Centro Clube.
+          </p>
+          <p>
+            
+Depois do concerto inaugural do baterista norte-americano Luigi Waites, a 8 de agosto de 1986 e dos da cantora Ellen Demos, a 21 de agosto do ano seguinte, e do grupo liderado pelo guitarrista Miguel Fevereiro (um valadense), no verão de 1988, houve que esperar quatro anos até que tivesse lugar a primeira edição do festival (1992) e depois outros sete para que chegasse a segunda (1999).
+          </p>
+          <code> #LuzSempreMaisLuz</code>
+      </section>      
+    </main>
+  </body>
+</div>
+)
+}
+
+export default App;
+
+/* Isto é um commit */
